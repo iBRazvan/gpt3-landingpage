6ceef0ad501381369745c37362e85e9ecf09fd9b
irazvanbotezatu <rezzy_botez@yahoo.com>
2023-03-27 20:43:58 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import { Brand, Navbar, CTA } from "./components";
+import "./App.css";
+
+// BEM --> Block Element Modifier
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
